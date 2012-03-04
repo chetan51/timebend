85e528926575d085aa0e180b9db5d9175afe2d3b
Chetan Surpur <chetan51@gmail.com>
2012-03-04 01:16:23 -0800

    Trying something else

--- javascripts/app.js
+++ javascripts/app.js
@@ -1976,7 +1976,8 @@
         _this = this;
       transform_properties = {
         '-webkit-transform-origin': '50% top 0',
-        '-webkit-transform': 'rotateX(' + rotate_x + 'deg)'
+        '-webkit-transform': 'rotateX(' + rotate_x + 'deg)',
+        'padding-bottom': height
       };
       if (animated) {
         return this.el.animate(transform_properties, {
