39f2353e204d6bb6b564c415baca47165d6f563c
Chetan Surpur <chetan51@gmail.com>
2012-03-04 23:44:26 -0800

    Redeploy

--- javascripts/app.js
+++ javascripts/app.js
@@ -2127,7 +2127,7 @@
       var dx, now;
       dx = this.last_touch.x - this.touch_start.x;
       now = new Date();
-      if (!this.hovering && (now - this.touch_start.time < this.config.touch_tap_time_tolerance) && (Math.abs(dx) < this.config.touch_tap_dist_tolerance)) {
+      if (!this.hovering && !app.global_scrolling && (now - this.touch_start.time < this.config.touch_tap_time_tolerance) && (Math.abs(dx) < this.config.touch_tap_dist_tolerance)) {
         this.transformTranslateX(0);
         if (event.target === this.duration[0]) {
           this.toggleDuration();
